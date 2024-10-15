a59d158f4ef9f4fec27c976feb27bcadd5df331e
Parimal <Parimal.c@quickheal.com>
2024-10-15 19:43:04 +0530

    invoice

--- src/main/java/com/ashtarang/controller/InvoiceController.java
+++ src/main/java/com/ashtarang/controller/InvoiceController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/assessment/v1")
+@RequestMapping("/invoice/v1")
 @CrossOrigin(value = "*")
 public class InvoiceController {
 
