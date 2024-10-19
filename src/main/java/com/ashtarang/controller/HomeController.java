4c75e3809b3a4b537cf32e2d0042b1e41d311806
Parimal <Parimal.c@quickheal.com>
2024-10-19 20:04:44 +0530

    security removed

---
+++ src/main/java/com/ashtarang/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.ashtarang.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(value = "*")
+public class HomeController {
+
+	@GetMapping("/")
+	public ResponseEntity<String> getInfo(){
+		String str  = "Hello World!";
+		System.out.println(str);
+		return ResponseEntity.status(200).body(str);
+	}
+}
